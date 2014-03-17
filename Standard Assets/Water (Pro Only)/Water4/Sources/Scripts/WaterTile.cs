79aea2a40844ea4d2d57f127fcf8d986fed6bb1a
Richard Frangenberg <richardf.1@web.de>
2014-03-18 00:39:48 +0100

    creation part9

---
+++ Standard Assets/Water (Pro Only)/Water4/Sources/Scripts/WaterTile.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class WaterTile : MonoBehaviour 
+{
+	public PlanarReflection reflection;
+	public WaterBase waterBase;
+	
+	public void Start () 
+	{
+		AcquireComponents();
+	}
+	
+	private void AcquireComponents() 
+	{
+		if (!reflection) {
+			if (transform.parent)
+				reflection = (PlanarReflection)transform.parent.GetComponent<PlanarReflection>();
+			else
+				reflection = (PlanarReflection)transform.GetComponent<PlanarReflection>();	
+		}
+		
+		if (!waterBase) {
+			if (transform.parent)
+				waterBase = (WaterBase)transform.parent.GetComponent<WaterBase>();
+			else
+				waterBase = (WaterBase)transform.GetComponent<WaterBase>();	
+		}
+	}
+	
+#if UNITY_EDITOR
+	public void Update () 
+	{
+		AcquireComponents();
+	}
+#endif
+	
+	public void OnWillRenderObject() 
+	{
+		Profiler.BeginSample("1");
+		if (reflection)
+			reflection.WaterTileBeingRendered(transform, Camera.current);
+		Profiler.EndSample();
+		Profiler.BeginSample("1");
+		if (waterBase)
+			waterBase.WaterTileBeingRendered(transform, Camera.current);
+		Profiler.EndSample();
+	}
+}
